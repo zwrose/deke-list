5babcd59c40b78fec402e83a927f38d3d4a5e596
Zach Rose <zwrose@gmail.com>
2014-03-21 09:23:45 -0400

    sails version to 0.9.13, initial User model creation

---
+++ api/models/User.js
@@ -0,0 +1,58 @@
+/**
+ * User.js
+ *
+ * @description :: A basic model of a user.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	schema: true,
+
+  attributes: {
+
+  	email: {
+  		type: 'string',
+  		email: true,
+  		required: true,
+  		unique: true
+  	},
+
+  	encryptedPassword: {
+  		type: 'string'
+  	},
+
+    admin: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.encryptedPassword;
+      return obj;
+    }
+
+  },
+
+
+  beforeValidation: function (values, next) {
+    // nothing here for the moment
+  },
+
+  beforeCreate: function (values, next) {
+
+    // This checks to make sure the password and password confirmation match before creating record
+    if (!values.password || values.password != values.confirmation) {
+      return next({err: ["Password doesn't match password confirmation."]});
+    }
+
+    require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encryptedPassword) {
+      if (err) return next(err);
+      values.encryptedPassword = encryptedPassword;
+      // values.online= true;
+      next();
+    });
+  }
+
+};
