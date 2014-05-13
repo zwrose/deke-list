a85a94f7d8c3d3613ca50dc65d1eda9e6b9284de
zwrose <zwrose@gmail.com>
2014-05-12 20:24:58 -0400

    updated newrelic logging level to info

--- newrelic.js
+++ newrelic.js
@@ -19,7 +19,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level : 'trace'
+    level : 'info'
   },
   rules : {
     ignore : [
