a4f33ea6501181cc3b990a7ec8f4eb8b55e88001
zwrose <zwrose@gmail.com>
2014-10-13 01:50:58 +0000

    adjusted app.js, added .salsrc

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Start sails and pass it command line arguments
 require('newrelic');
-var config = sails.util.merge(require('optimist').argv,{
-  hooks: {
-    sockets: false,
-    pubsub: false 
-  }
-});
-require('sails').lift(config);
+// var config = sails.util.merge(require('optimist').argv,{
+//   hooks: {
+//     sockets: false,
+//     pubsub: false 
+//   }
+// });
+// require('sails').lift(config);
+require('sails').lift(require('optimist').argv);
